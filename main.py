7cfc34721bcbaec5cbc2e9a0cc4470d96510639a
wguan17 <wguan17@gmail.com>
2019-06-15 17:20:51 +0800

    Add total and count of each category

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from modules.analyzer import Analyzer
 
 
@@ -17,8 +18,15 @@ if __name__ == "__main__":
     analyzer = Analyzer()
     df['Category'] = pd.Series(df['Shop'].apply(analyzer.get_category), index=df.index)
     df['Date'] = pd.to_datetime(df['Date'])
-    print(df.groupby(['Category', 'Shop']).sum())
-    print(df.groupby('Category').sum().sort_values('Amount', ascending=False))
-    print(df.groupby('Date').sum())
+    df_total = pd.DataFrame(df.groupby('Category').agg({'Amount': np.sum, 'Category': np.size}))
+    df = pd.merge(left=df, right=df_total,
+                  how='left', left_on='Category', right_index=True)
+
+    # df = df.set_index(['Date', 'Category'])
+    # print(df.groupby(['Category']).sum())
+    # print(df.groupby('Category').sum().sort_values('Amount', ascending=False))
+    # print(df.groupby('Date').sum())
+    df = df.pivot_table(index=['Category_x', 'Amount_y', 'Category_y', 'Shop'], aggfunc=np.sum)
+    print(df.sort_values(['Amount_y', 'Amount_x'], ascending=False))
 
 
