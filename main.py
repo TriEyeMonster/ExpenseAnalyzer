b9cb652c2ce345a75a36881cdc5f9155ae17344d
wguan17 <wguan17@gmail.com>
2019-06-18 22:13:39 +0800

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from modules.analyzer import Analyzer
 if __name__ == "__main__":
 
     src_file = r"Data/May.xls"
+    analyzer = Analyzer()
     df = pd.read_excel(src_file, skiprows=range(1, 9), header=1)
     df = df[~df.Description.isin(['GIRO PAYMENT                            ', 'Previous Balance'])]
     df[['shop', 'ref_no']] = df.Description.str.split('Ref No:', 1, expand=True)
@@ -15,7 +16,6 @@ if __name__ == "__main__":
     df.shop = df.shop.str.split(r'\(', expand=True)
     df.shop = df.shop.str.strip()
     df.columns = ['Date', 'Amount', 'Shop']
-    analyzer = Analyzer()
     df['Category'] = pd.Series(df['Shop'].apply(analyzer.get_category), index=df.index)
     df['Date'] = pd.to_datetime(df['Date'])
     df_total = pd.DataFrame(df.groupby('Category').agg({'Amount': np.sum, 'Category': np.size}))
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     # print(df.groupby(['Category']).sum())
     # print(df.groupby('Category').sum().sort_values('Amount', ascending=False))
     # print(df.groupby('Date').sum())
-    df = df.pivot_table(index=['Category_x', 'Amount_y', 'Category_y', 'Shop'], aggfunc=np.sum)
-    print(df.sort_values(['Amount_y', 'Amount_x'], ascending=False))
+    df.columns = ['Date', 'Amount', 'Shop', 'Category', 'Count', 'Total']
+    df = df.pivot_table(index=['Category', 'Total', 'Count', 'Shop'], aggfunc=np.sum)
+    print(df.sort_values(['Total', 'Amount'], ascending=False))
 
 
